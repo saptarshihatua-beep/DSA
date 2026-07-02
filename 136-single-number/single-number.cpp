91b0cee5096b2306a69126bada74142a6d2126e8
saptarshihatua-beep <saptarshihatua@gmail.com>
2026-07-02 23:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (98.82%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+         for(int i = 0; i< nums.size(); i++){
+            int val =nums[i];
+            ans = ans ^ val;
+         }
+         return  ans ;
+
+        
+    }
+};
