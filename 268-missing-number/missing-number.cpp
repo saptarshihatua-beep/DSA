c51b64cff71b0e452849f37606e2e0bc88f5bc3f
Saptarshi Hatua <saptarshihatua@gmail.com>
2026-07-25 00:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (99.82%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n =nums.size();
+        int actualsum=0;
+        int expectedsum=n*(n+1)/2;
+        for(int i=0;i<n;i++){
+            actualsum+=nums[i];
+
+        }
+        return expectedsum-actualsum;
+        
+    }
+};
